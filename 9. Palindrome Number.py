c86dfc331f1853ba1e6f98573b157a3d51b571f1
Husnain Tarar <husnaintararr@gmail.com>
2026-09-08 20:46:50 +0500

    Add new program

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -2,5 +2,5 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
-        s = str(x)
-        return s == s[::-1]
+        s = str(x) # convert the integer to a string
+        return s == s[::-1] # check if the string is equal to its reverse
